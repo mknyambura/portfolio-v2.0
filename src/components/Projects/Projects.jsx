673f75df22f6948183575948a81f28c28c6a4936
Mercy Nyambura Kariuki <kariuki.mercie@gmail.com>
2024-01-07 22:57:15 +0000

    Update Projects.jsx

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,63 +1,26 @@
-// import {useState, useEffect} from 'react'
-// import Navbar from '../Navbar/Navbar'
-// import Footer from '../Footer/Footer'
-// import './Projects.scss'
-// import data from '../../../projects.json'
-
-// function Projects() {
-//     const [projects, setProjects] = useState([])
-
-//     useEffect(() => {
-//       fetch('./allprojects')
-//         .then((response) => response.json())
-//         .then(setProjects)
-//     }, [])
-    
-//   return (
-//     <div className='page'>
-//         <div className='w-1/2 pt-[50px]'>
-//             <Navbar/>
-//             <div className='pt-[50px]'>
-//                 <div className='flex flex-col gap-8'>
-//                     <h1 className='font-bold text-4xl'>Projects</h1>
-//                     <h1 className='text-[20px]'>Programming is like unraveling a puzzle, and sometimes the most efficient way to tackle it is to take a step back and consider the broader view. - Mercy Kariuki</h1>
-//                 </div>
-                
-//                 <div>
-                //    {data.allprojects.map((project) => (
-                //        <div key={project.id} className='scale-100 hover:transition-all hover:duration-500 hover:ease-in-out hover:shadow-xl hover:shadow-slate-400 hover:scale-110 m-[20px] p-[20px] gap-4 rounded-xl' >
-                //            <div className='flex flex-row justify-between gap-8 mb-[10px]'>
-                //                <h1 className='font-semibold text-[25px]'>{project.title}</h1>
-                //                <h1><a href={project.code} className='hover:transition-all hover:duration-500 text-[#a6a8ab] hover:text-[#333333] hover:text-xl hover:underline'>Code</a></h1>
-                //            </div>
-                //            <div className='mb-[10px]'>
-                //                 <p className='text-[20px]'>{project.description}</p>
-                //             </div>
-                //            <ul className='mb-[10px] flex flex-col list-disc marker:text-[#FF9B00] gap-1'>
-                //                <li>{project.tool1}</li>
-                //                <li>{project.tool2}</li>
-                //                {/* <li>{project.tool3}</li> */}
-                //                {/* <li>{project.tool4}</li> */}
-                //            </ul>
-                //            <hr className='text-[#a6a8ab] bg-[#a6a8ab] h-[2px] mt-[2px]'/>
-                //        </div>
-                //    ))}
-//                 </div>
-//             </div>
-//             <Footer/>
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default Projects
-
-// import React from 'react'
+import {useState} from 'react'
 import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer'
 import data from '../../../db.json'
 
 function Projects() {
+    const [visibleProjects, setVisibleProjects] = useState(5);
+    const [showReadLess, setShowReadLess] = useState(false);
+  
+    const loadMoreProjects = () => {
+      const newVisibleProjects = visibleProjects + 5;
+      setVisibleProjects(newVisibleProjects);
+  
+      if (newVisibleProjects >= data.allprojects.length) {
+        setShowReadLess(true);
+      }
+    };
+  
+    const loadLessProjects = () => {
+      setVisibleProjects(5);
+      setShowReadLess(false);
+    };
+
   return (
     <div className='page'>
         <div className='w-1/2 pt-[50px]'>
@@ -69,7 +32,7 @@ function Projects() {
                 </div> 
 
                 <div>
-                    {data.allprojects.map((project) => (
+                    {data.allprojects.slice(0, visibleProjects).map((project) => (
                         <div key={project.id} className='scale-100 hover:transition-all hover:duration-500 hover:ease-in-out hover:shadow-xl hover:shadow-slate-400 hover:scale-110 m-[20px] p-[20px] gap-4 rounded-xl' >
                             <div className='flex flex-row justify-between gap-8 mb-[10px]'>
                                 <h1 className='font-semibold text-[25px]'>{project.title}</h1>
@@ -88,6 +51,28 @@ function Projects() {
                         </div>
                      ))}
                 </div>
+
+                {showReadLess && (
+                    <div className='flex gap-4'>
+                      <button
+                        className='text-[#a6a8ab] hover:text-[#333333] hover:underline'
+                        onClick={loadLessProjects}
+                      >
+                        Read Less
+                      </button>
+                    </div>
+                )}
+
+                {visibleProjects < data.allprojects.length && (
+                  <div className='flex gap-4'>
+                    <button
+                      className='text-[#a6a8ab] hover:text-[#333333] hover:underline'
+                      onClick={loadMoreProjects}
+                    >
+                      Read More
+                    </button>
+                  </div>
+                )}
             </div>
             <Footer/>
         </div>
