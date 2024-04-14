977ae101297d2a8345c099d1343abfbca6f99605
Mercy Nyambura Kariuki <kariuki.mercie@gmail.com>
2024-04-14 23:06:07 +0100

    update about me

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,23 +11,25 @@ function About() {
             <Navbar/>
             <div className='about flex flex-col'>
                 <div className='flex flex-col gap-8 pt-[50px]'>
-                    <h1 className='text-[50px] font-bold text-[#333333]'>Mercy F Nyambura Kariuki.</h1>
-                    <h3 className='text-xl'>
+                    <h1 className='text-[30px] font-bold text-[#333333]'>About Me</h1>
+                    {/* <h3 className='text-xl'>
                         <a href='https://www.tees.ac.uk/Postgraduate_courses/Computing_&_Cyber_Security/msc_data_science.cfm' className='underline'>Msc. Applied Data Science (with Advanced Practice)</a> + Big Data & Intelligence Lab Assistant
                         <a href="https://www.tees.ac.uk/" className='underline'> @ Teesside University</a>
-                    </h3>
+                    </h3> */}
                     
                 </div>
                 <div className='flex flex-row justify-between gap-8 items-center pt-[100px]'>
-                    <img src={dp} alt="dp" width={450} height={450}/>
+                    <img src={dp} alt="dp" className='w-1/2 h-1/2'/>
                     <div className='flex flex-col items-center gap-8'>
-                        <p className='text-xl'>
-                            I am a data scientist/machine learning engineer with a background in computer science, economics and mathematics
-                            I am currently a student at Teesside University, pursuing a Masters in Applied Data Science
+                        <p>
+                            Data Scientist with 1+ years of experience in Analytics, Strategy and Project Management. A Masters in Applied Data Science from Teesside University. Currently at Spot2Spot Network Services. Previously at Teesside, Kenya Revnue Authority (KRA), Civil Registration Services and Kenya Power Lighting Ltd (KPLC).
                         </p>
-                        <a href='/resume'>
+                        <p>
+                            Invloved in East African Society as the Vice Chair of Communication and an Instructor at Microsoft Tesside University PowerBI by Women for Women Fabric Group
+                        </p>
+                        {/* <a href='/resume'>
                             <button className='text-[18px] text-[#7e7e7e] hover:transition-all hover:duration-500 hover:ease-in-out hover:text-[22px] rounded-[20px] hover:underline hover:text-black ml-[20px] mr-[20px] mt-[10px] mb-[10px]'>Resume</button>
-                        </a>
+                        </a> */}
                     </div>
                 </div>
             </div>
